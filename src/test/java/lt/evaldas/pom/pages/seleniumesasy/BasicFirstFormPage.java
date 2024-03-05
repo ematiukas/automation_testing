5120336459138bb79b6d749d7a1ab2e132a82850
ematiukas <evaldas@techzity.com>
2024-03-05 21:26:06 +0200

    Add TestDemoSeleniumClass

---
+++ src/test/java/lt/evaldas/pom/pages/seleniumesasy/BasicFirstFormPage.java
@@ -0,0 +1,46 @@
+package lt.evaldas.pom.pages.seleniumesasy;
+
+import lt.evaldas.pom.pages.Common;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BasicFirstFormPage {
+    public static void open(String url) {
+        Common.setUpChrome();
+        Common.openUrl(url);
+    }
+
+    public static void close() {
+
+    }
+
+    public static void writeMessage(String message) {
+        Common.sendKeysToElement(
+                By.xpath("//input[@id='user-message']"), message
+        );
+    }
+
+    public static void clickOnButtonShowMessage() {
+        Common.clickOnElement(By.xpath("//button[@onclick='showInput();']"));
+    }
+
+    public static String readMessage() {
+        return Common.getTextFromElement(By.xpath("//span[@id='display']"));
+    }
+
+    public static void writeValueA(String value) {
+        Common.sendKeysToElement(By.xpath("//input[@id='value1']"),value);
+    }
+
+    public static void writeValueB(String value) {
+        Common.sendKeysToElement(By.xpath("//input[@id='value2']"),value);
+    }
+
+    public static void clickOnButtonGetTotal() {
+        Common.clickOnElement(By.xpath("//button[@onclick='return total()']"));
+    }
+
+    public static String readSumTotal() {
+        return Common.getTextFromElement(By.xpath("//span[@id='displayvalue']"));
+    }
+}
