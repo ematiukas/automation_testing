6290bc7d1a2b99af458e7392606705ca4b90b175
ematiukas <evaldas@techzity.com>
2024-03-06 16:42:47 +0200

    Fix Driver class

--- src/test/java/lt/evaldas/pom/utils/Driver.java
+++ src/test/java/lt/evaldas/pom/utils/Driver.java
@@ -13,20 +13,20 @@ public class Driver {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
+
         options.addArguments("start-maximized");
-        options.addArguments("--force-device-scale-factor=0.75");
+        options.addArguments("--force-device-scale-factor=0.85");
 //        options.addArguments("--headless=new");
 //        options.addArguments("window-size=5000,3000");
 
         chromeDriver = new ChromeDriver(options);
-        chromeDriver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
     }
 
     public static WebDriver getChromeDriver() {
         return chromeDriver;
     }
 
-    public static void quitChromeDriver(){
+    public static void quitChromeDriver() {
         chromeDriver.quit();
-    }
+            }
 }
