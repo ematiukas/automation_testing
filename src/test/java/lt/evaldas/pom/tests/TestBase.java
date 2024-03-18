b56c0da3d2e047383cea11f2391e0a3f38ad0d29
ematiukas <evaldas@techzity.com>
2024-03-18 21:02:38 +0200

    Add TestListener

--- src/test/java/lt/evaldas/pom/tests/TestBase.java
+++ src/test/java/lt/evaldas/pom/tests/TestBase.java
@@ -1,9 +1,12 @@
 package lt.evaldas.pom.tests;
 
 import lt.evaldas.pom.pages.Common;
+import lt.evaldas.pom.utils.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class TestBase {
     @BeforeMethod
     public abstract void setUp();
---
+++ src/test/java/lt/evaldas/pom/utils/TestListener.java
@@ -0,0 +1,36 @@
+package lt.evaldas.pom.utils;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestListener implements ITestListener {
+    private void takeScreenshot() {
+        TakesScreenshot screenshot = (TakesScreenshot) Driver.getChromeDriver();
+        File screenshotFile = screenshot.getScreenshotAs(OutputType.FILE);
+
+        String directory = "./screenshots/";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HH_mm_ss_SS");
+        File saveFile = new File("%s%s_screenshot.png".formatted(directory, LocalDateTime.now().format(formatter)));
+
+        try {
+            FileUtils.copyFile(screenshotFile, saveFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        takeScreenshot();
+    }
+}
